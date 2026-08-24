319a4871c231c860384dafb05314ff32f968e7eb
XianYun <2057914965@qq.com>
2026-08-25 00:07:34 +0800

    修复基础场景渲染与相机位置

---
+++ script.js
@@ -0,0 +1,34 @@
+const viewSize = {
+    width:800,
+    height:600
+}
+
+
+//场景
+const scene  = new THREE.Scene();
+
+//几何体
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//材质
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+//三维网格
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+//相机
+/*THREE.PerspectiveCamera(params1,params2);
+    params1是FOV（Field Of View），即视野。视野越小，看地越远。视野越大，看得越多，边缘越畸形。
+    parmas2是宽高比：宽除以高度
+*/
+const camera = new THREE.PerspectiveCamera(75, viewSize.width / viewSize.height);
+camera.position.z = 3;
+scene.add(camera);
+
+
+const canvas = document.querySelector(".webgl");
+//渲染器
+const renderer = new THREE.WebGLRenderer({
+    canvas:canvas
+});
+renderer.setSize(viewSize.width, viewSize.height);
+renderer.render(scene, camera);
