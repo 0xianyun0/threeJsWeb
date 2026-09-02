d41a9e75cdea463c8fce0a2c7d8de1272a4a130b
Jin Xiaoming <2057914965@qq.com>
2026-09-02 18:31:59 +0800

    开始学习动画

---
+++ 05-Animations/script.js
@@ -0,0 +1,73 @@
+import * as THREE from "three"
+const canvas = document.getElementById("webgl");
+
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry();
+
+const meshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000,});
+const mesh = new THREE.Mesh(geometry,meshBasicMaterial);
+
+
+console.log("场景中心到物体的距离（归一之前）：" + mesh.position.length())
+mesh.position.normalize()
+console.log("场景中心到物体的距离（归一之后）：" + mesh.position.length())
+
+const axesHelper = new THREE.AxesHelper(1);
+
+
+
+
+
+scene.add(axesHelper);
+scene.add(mesh);
+
+
+const size = {
+    width:800,
+    height:600
+}
+const perspectiveCamera = new THREE.PerspectiveCamera(75,size.width/size.height);
+//mesh.position.distanceTo(perspectiveCamera.position) 相机到物体之间的距离
+console.log("相机到物体的距离（z = 0）：" + mesh.position.distanceTo(perspectiveCamera.position))
+
+
+perspectiveCamera.position.z = 4
+
+
+console.log("相机到物体的距离（z = 3）：" + mesh.position.distanceTo(perspectiveCamera.position))
+
+
+scene.add(perspectiveCamera)
+
+const webGLRenderer = new THREE.WebGLRenderer({
+    canvas
+});
+
+webGLRenderer.setSize(size.width,size.height)
+
+let time = Date.now();
+/*
+通过帧的回调函数，来制作动画。
+window.requestAnimationFrame 每帧输出的重绘之前会被浏览器调用
+ */
+const tick = ()=>{
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+    console.log(deltaTime)
+
+    console.log("tick is running")
+    // mesh.position.x += 0.001
+    // mesh.position.y += 0.001
+    // mesh.position.z += 0.001
+    // mesh.scale.x += 0.001
+    mesh.rotation.x += 0.001 * deltaTime;
+    //渲染
+    webGLRenderer.render(scene,perspectiveCamera);
+
+    window.requestAnimationFrame(tick);
+}
+tick()
+
